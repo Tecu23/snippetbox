4abaa13fdc65df383ca4f29af2c9cf659add56b3
Tecu23 <andrei.teculescu@gmail.com>
2024-02-23 20:35:35 +0200

    add some routes

--- main.go
+++ main.go
@@ -8,7 +8,25 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox as the response body. "
 func home(w http.ResponseWriter, r *http.Request) {
+  // Check if the current request URL path exactly matches "/". If it doesn't use 
+  // the http.NotFound() function to send a 404 response to the client.
+  // Importantly, we then return from the handler. If we don;t return the handler
+  // would keep executing and also write the "Hello from SnippetBox" message.
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
   w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function.
+func snipperView(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Display a specific snipper..."))
+}
+
+// Add a snippetCreate handler function. 
+func snippetCreate (w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Create a new snippet..."))
 }
 
 func main(){
@@ -16,6 +34,8 @@ func main(){
   // register the home function as the handler for the "/" URL pattern
   mux := http.NewServeMux()
   mux.HandleFunc("/", home);
+  mux.HandleFunc("/snippet/view/", snipperView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
 
   // Print a log message to say the server is starting.
   log.Print("Starting server on :4000")
