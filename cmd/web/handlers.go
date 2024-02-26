0b7f703c34d03ac87664c426637fb40bcc81ba36
Tecu23 <andrei.teculescu@gmail.com>
2024-02-26 21:11:59 +0200

    add some error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,12 +5,11 @@ import (
 	"net/http"
 	"strconv"
 	"text/template"
-  "log"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
-    http.NotFound(w, r)
+    app.notFound(w)
     return
   }
 
@@ -27,8 +26,7 @@ func home(w http.ResponseWriter, r *http.Request) {
   // of the file slice as variadic arguments.
   ts, err := template.ParseFiles(files...)
   if err != nil {
-    log.Print(err.Error())
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    app.serverError(w, r, err)
     return
   }
 
@@ -36,26 +34,25 @@ func home(w http.ResponseWriter, r *http.Request) {
   // template as the response body.
   err = ts.ExecuteTemplate(w, "base", nil)
   if err != nil {
-    log.Print(err.Error())
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    app.serverError(w, r, err)
   }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
   id, err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
-    http.NotFound(w, r)
+    app.notFound(w)
     return
   }
 
   fmt.Fprintf(w, "Dipslay a specific snipper with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
   if r.Method != http.MethodPost {
     w.Header().Set("Allow", http.MethodPost)
-    http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+    app.clientError(w, http.StatusMethodNotAllowed)
     return
   }
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type application struct {
+  logger *slog.Logger
+}
+
 func main() {
+  addr :=  flag.String("addr", ":4000", "HTTP network address")
+
+  flag.Parse()
+
+  logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+  // Initialize a new instance of out application struct
+  app := &application{
+    logger: logger,
+  }
+
   mux := http.NewServeMux()
 
   fileServer := http.FileServer(http.Dir("./ui/static/"))
 
   mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-  mux.HandleFunc("/", home)
+  mux.HandleFunc("/", app.home)
 
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+  mux.HandleFunc("/snippet/view", app.snippetView)
+  mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-  log.Print("Starting server on :4000")
+  logger.Info("Starting server", "addr", *addr)
+  err := http.ListenAndServe(*addr, mux)
 
-  err := http.ListenAndServe(":4000", mux)
-
-  log.Fatal(err)
+  logger.Error(err.Error())
+  os.Exit(1)
 }
