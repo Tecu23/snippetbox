3d52f3370f674e9d0cecd1c7b547f36b98a466b3
Tecu23 <andrei.teculescu@gmail.com>
2024-02-28 13:01:02 +0200

    change the default router

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,13 +7,11 @@ import (
 	"strconv"
 
 	"github.com/Tecu23/snipperbox/internal/models"
+
+  "github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path != "/" {
-    app.notFound(w)
-    return
-  }
 
   snippets, err := app.snippets.Latest()
   if err !=  nil {
@@ -29,7 +27,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
-  id, err := strconv.Atoi(r.URL.Query().Get("id"))
+  params := httprouter.ParamsFromContext(r.Context())
+
+  id, err := strconv.Atoi(params.ByName("id"))
   if err != nil || id < 1 {
     app.notFound(w)
     return
@@ -51,13 +51,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
   app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
-
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  if r.Method != http.MethodPost {
-    w.Header().Set("Allow", http.MethodPost)
-    app.clientError(w, http.StatusMethodNotAllowed)
-    return
-  }
+  w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
   title := "O snail"
   content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
